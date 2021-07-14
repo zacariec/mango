4db55388d1682d49de0195acedaa91fa6f55f4ca
ZacarieCarr <zacarie.carr@acidgreen.com.au>
2021-07-14 17:05:29 +1000

    resolved webpack

--- bin/lib/build/build.js
+++ bin/lib/build/build.js
@@ -61,7 +61,7 @@ const buildDistFiles = async () => {
         await copyToDist(_Directorys.templatesRoot, _Directorys.distTemplatesRoot, 'Templates');
         await sleep(500);
         const webPackSpinner = ora('Packing styles and scripts with Webpack').start();
-        const command = spawn('webpack', ['--config', `${_Directorys.projectRoot}/webpack.production.config`]);
+        const command = spawn('npx', ['webpack', '--config', `${_Directorys.projectRoot}/webpack.production.config`]);
         command.stdout.on('data', data => {
             spawnCallback(data, false)
             if(data.toString().includes('ERROR')) {
