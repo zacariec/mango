b56f934da3ea0533bec3c01f7ad048ad1007f86c
zacariecarr <zacarie.carr@acidgreen.com.au>
2022-05-19 18:59:05 +1000

    Refactored getPublished to allow for support of environment being passed.

--- bin/lib/shopify/getPublished.ts
+++ bin/lib/shopify/getPublished.ts
@@ -3,8 +3,8 @@ import { Theme } from "../../../types/types";
 
 const getPublishedTheme = (themes: []): Theme => <Theme>themes.find((theme: Theme) => theme.role === 'main' && theme);
 
-const getPublished = async (): Promise<Theme> => {
-  const { themes } = await getThemes();
+const getPublished = async (environment): Promise<Theme> => {
+  const { themes } = await getThemes(environment);
 
   return getPublishedTheme(themes);
 };
