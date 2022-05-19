e1b6241f6699315a48a7b918552e54b2203e5a10
zacariecarr <zacarie.carr@acidgreen.com.au>
2022-05-19 18:52:35 +1000

    Added explicit type for the parameter passed to sleep.

--- bin/lib/utils/_sleep.ts
+++ bin/lib/utils/_sleep.ts
@@ -1,4 +1,4 @@
-const sleep = async (ms): Promise<Promise<void>> => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = async (ms: number): Promise<Promise<void>> => new Promise(resolve => setTimeout(resolve, ms));
 
 export default sleep;
 
