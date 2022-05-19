68d00063b05c0d3dd053357dc7e80beaf1f4ffb7
zacariecarr <zacarie.carr@acidgreen.com.au>
2022-05-19 18:59:50 +1000

    Refactored how the initializeWatchers method is ran. Added explicit types. Changes to steps to run instead of chaining .then()

--- bin/lib/index.ts
+++ bin/lib/index.ts
@@ -2,25 +2,38 @@ import { cloneDirectory } from './utils/_fsUtils';
 import { createDevDirectory, moveAssetsToDev } from './convert/convert';
 import deployThemeFile from './deploy/deploy';
 import downloadThemeFiles from './download/download';
-import { liveReload } from './reload/liveReload.js';
+import { liveReload, removeLiveReload } from './reload/liveReload.js';
 import { initializeWebpack, initializeThemekit, initializeWorkingDirectory } from './watch/watch';
 import getVersion from './version/version';
+import _randomPort from './utils/_createPort';
 
 // TODO: Handle console.errors(), probably prettify output with
 // chalk and a handler that takes in a callback etc - across all modules.
 
-const createWorkingDirectory = async () => {
+const createWorkingDirectory = async (): Promise<void> => {
   await cloneDirectory();
   await createDevDirectory();
   await moveAssetsToDev();
 };
 
-const initializeWatchers = () => {
-  initializeThemekit()
-    .then(() => liveReload())
-    .then(() => initializeWebpack())
-    .then(() => initializeWorkingDirectory())
-    .catch(err => console.error(err));
+const initializeWatchers = async (): Promise<void> => {
+  const port = await _randomPort();
+  const stepsToRun = [
+    initializeWebpack,
+    initializeWorkingDirectory,
+  ];
+  await initializeThemekit(port);
+  await removeLiveReload();
+  await liveReload(port);
+
+  try {
+    for await (const step of stepsToRun) {
+      await step();
+    }
+  } catch (err) {
+    console.error(err);
+    return process.exit(1);
+  }
 };
 
 export {
