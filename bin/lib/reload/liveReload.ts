a73ea84b35d25d4f64231f312c4f773e6172c7d5
zacariecarr <zacarie.carr@acidgreen.com.au>
2022-05-19 19:01:59 +1000

    Added the removeLiveReload func to remove liveReload from the layout files. Refactored the liveReload func to support multiple ports and multiple websockets. Added support for all layout files.

--- bin/lib/reload/liveReload.ts
+++ bin/lib/reload/liveReload.ts
@@ -1,40 +1,45 @@
 import fs from 'fs-extra';
 import path from 'path';
 
-const removeLiveReload = async () => {
+// Function to remove the live reload from the layout files.
+const removeLiveReload = async (): Promise<void> => {
+  const regexString = /<!-- Start Live Server Reloading -->([\s\S]*?)<!-- End Live Server Reloading -->/g;
   try {
-    const files = await fs.readdir(path.resolve('./shop/src/layout'));
-    // files.forEach((file) => {
-    //   const fileData = await
-    // });
-    const fileData = await fs.readFile(path.resolve('./shop/src/layout/theme.liquid'));
-    if (fileData.toString().match(/<!-- Start Live Server Reloading -->([\s\S]*?)<!-- End Live Server Reloading -->/g)) {
-      const dataToWrite = fileData.toString().replace(/<!-- Start Live Server Reloading -->([\s\S]*?)<!-- End Live Server Reloading -->/g, '');
-      await fs.writeFile(path.resolve('./shop/src/layout/theme.liquid'), dataToWrite);
+    const files: string[] = await fs.readdir(path.resolve('./shop/src/layout'));
+    for await (const file of files) {
+      const fileData: Buffer = await fs.readFile(`./shop/src/layout/${file}`);
+      if (fileData.toString().match(regexString)) {
+        const dataToWrite: string = fileData.toString().replace(regexString, '');
+        await fs.writeFile(path.resolve(`./shop/src/layout/${file}`), dataToWrite);
+      }
     }
   } catch (err) {
     return console.error(err);
   }
 }
 
-const liveReload = async () => {
-  try {
-    const fileData = await fs.readFile(path.resolve('./shop/src/layout/theme.liquid'));
-    if (fileData.toString().includes('<!-- Start Live Server Reloading -->') === false) {
-      const fileArray = fileData.toString().split('</body>');
-      const dataToWrite = `
-                ${fileArray[0]}
-                    <!-- Start Live Server Reloading -->
-                    <script>
-                        var ws = new WebSocket('ws://localhost:9000');
-                        ws.onmessage = function(evt) { location.reload(); }
-                    </script>
-                    <!-- End Live Server Reloading -->
-                </body>
-                ${fileArray[1]}
-            `;
+// Function to add the websocket/live reload functionality to layout files.
+const liveReload = async (port: number): Promise<void> => {
+  const websocketPort = `ws://localhost:${port}`;
 
-      await fs.writeFile(path.resolve('./shop/src/layout/theme.liquid'), dataToWrite);
+  try {
+    const files = await fs.readdir(path.resolve('./shop/src/layout'));
+    for await (const file of files) {
+      const fileData = await fs.readFile(path.resolve(`./shop/src/layout/${file}`));
+      if (fileData.toString().includes('<!-- Start Live Server Reloading -->') === false) {
+        const fileArray = fileData.toString().split('</body>');
+        const dataToWrite = `${fileArray[0]}
+          <!-- Start Live Server Reloading -->
+          <script>
+            var ws = new WebSocket('${websocketPort}');
+            ws.onmessage = function(evt) { location.reload(); }
+          </script>
+          <!-- End Live Server Reloading -->
+        </body>
+        ${fileArray[1]}`;
+
+        await fs.writeFile(path.resolve(`./shop/src/layout/${file}`), dataToWrite);
+      }
     }
   } catch (err) {
     return console.error(err);
