b1832e51491c09e8267388ce144f850828af8734
zacariecarr <zacarie.carr@acidgreen.com.au>
2022-05-19 18:52:17 +1000

    Changed from default export to exports in favour of adding the getConfigFromEnvironment method.

--- bin/lib/utils/_readThemeConfig.ts
+++ bin/lib/utils/_readThemeConfig.ts
@@ -1,6 +1,6 @@
 import yaml from 'js-yaml';
 import fs from 'fs-extra';
-import { StorefrontConfig } from "../../../types/types";
+import { StorefrontConfig, StoreEnvironment } from "../../../types/types";
 
 const readThemeConfig = async (): Promise<StorefrontConfig> => {
   try {
@@ -12,4 +12,25 @@ const readThemeConfig = async (): Promise<StorefrontConfig> => {
   }
 };
 
-export default readThemeConfig;
+const getConfigFromEnvironment = async (environment): Promise<StoreEnvironment> => {
+  const config = { ...await yaml.load(fs.readFileSync('config.yml')) };
+  let configToReturn = {
+    ...config[environment],
+    name: environment,
+  };
+
+  if (!config[environment]) {
+    console.error(`couldn't find the provided environment, falling back to development`);
+    configToReturn = {
+      ...config[Object.keys(config)[0]],
+      name: 'development',
+    };
+  }
+
+  return configToReturn;
+};
+
+export {
+  readThemeConfig,
+  getConfigFromEnvironment,
+};
